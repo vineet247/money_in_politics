461f290b5316630ecfedd0a1d489141690825a9f
vineet247 <vineetmilan@gmail.com>
2019-05-08 16:04:12 -0400

    new python file change.py added

--- project/change.py
+++ project/change.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 
-df = pd.DataFrame(pd.read_csv(r"C:\Users\vinee\Desktop\Github\money_in_politics\final.csv", sep = ";"))
+df =pd.read_csv(r"C:\Users\vinee\Desktop\Github\money_in_politics\final.csv", sep = ",")
 
 
-df.loc[df['ideology'] == "A", 'column_to_update'] = "Agriculture"
+df.loc[df['ideology'] == "Z", 'ideology'] = "committees"
+df.to_csv(r"C:\Users\vinee\Desktop\Github\money_in_politics\final.csv", sep = ",",index=False)
