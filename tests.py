7cd8921ef292dd15b963d51fee875694d6babfa0
vineet247 <vineetmilan@gmail.com>
2019-05-16 11:46:05 -0400

    updated files

--- tests.py
+++ tests.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
 
-dataset = pd.read_csv(r'clustered_party.csv')
+dataset = pd.read_csv(r'clustered_row_column.csv')
 
-df2 = pd.melt(dataset, id_vars=['Name', 'Party'], var_name="ideology",value_name="Amt")
+df2 = pd.melt(dataset, id_vars=['Name'], var_name="ideology",value_name="Amt")
 df2 = df2.fillna('0')
-df2.to_csv(r'project\new_clustered_3.csv')
+df2.to_csv(r'project\new_clustered_2.csv')
 print(df2)
