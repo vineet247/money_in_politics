e39783f8231d3565906d57d71aacd5211da691cb
vineet247 <vineetmilan@gmail.com>
2019-05-16 10:35:42 -0400

    added new html and css files for the new view. Added view for sorting by party

--- tests.py
+++ tests.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
 
-dataset = pd.read_csv(r'project\test.csv')
+dataset = pd.read_csv(r'clustered_party.csv')
 
-df2 = pd.melt(dataset, id_vars=['Name'], var_name="ideology",value_name="Amt")
+df2 = pd.melt(dataset, id_vars=['Name', 'Party'], var_name="ideology",value_name="Amt")
 df2 = df2.fillna('0')
-df2.to_csv(r'project\new_clustered_2.csv')
+df2.to_csv(r'project\new_clustered_3.csv')
 print(df2)
